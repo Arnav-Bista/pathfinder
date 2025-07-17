4665a12ef1f932e38abd539f550930c0ecb5d09f
Arnav-Bista <arnav.bista12@gmail.com>
2025-07-17 08:33:06 +0100

    initial

---
+++ frontend/src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import type { FormEvent } from 'react';
+import './App.css'
+import { Card } from './components/ui/card';
+import { Input } from './components/ui/input';
+import { Label } from './components/ui/label';
+
+export default function App() {
+  const [places, setPlaces] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      setPlaces([...places, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div className='px-8 max-w-screen-lg items-center justify-center mx-auto flex flex-col'>
+      <h1 className='my-4 text-6xl w-full text-center font-bold'>Pathfinder</h1>
+
+      <Card className='flex flex-col px-4 py-4 flex-grow max-w-screen-md w-full'>
+        <form onSubmit={handleSubmit}>
+          <Label htmlFor='lookup' className='my-2'>Where we headed?</Label>
+          <Input
+            id='lookup'
+            placeholder='123 Hope Street, St Andrews, UK'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </form>
+        <div className='flex flex-col '>
+          {places.map((place, index) => (
+            <div key={index} className='text-lg text-gray-700'>
+              {place}
+            </div>
+          ))}
+        </div>
+      </Card>
+
+
+    </div>
+  );
+}
